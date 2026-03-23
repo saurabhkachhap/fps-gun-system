c3ad5e4f40e9c1b1b962a36303c91cd1bd5b543e
Saurabh <saurabhkachhap122@gmail.com>
2026-03-23 19:48:16 +0530

    Decal system implementation

--- Assets/_Project/Scripts/HitScanShootingBehavior.cs
+++ Assets/_Project/Scripts/HitScanShootingBehavior.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Hit Scan", menuName = "Scriptable Objects/Shooting/Hitscan")]
 public class HitScanShootingBehavior : ScriptableObject, IShootingBehavior
 {
+    [SerializeField] private GameObject decalPrefab;
+
     public void Shoot(Vector3 origin, Vector3 direction, GunData data)
     {
         Ray ray = new Ray(origin, direction.normalized);
         if(Physics.Raycast(ray, out var hitInfo, data.Range, data.LayerMask))
         {
-            Debug.Log(hitInfo.collider.name);
+            var position = hitInfo.point;
+            Quaternion rotation = Quaternion.LookRotation(hitInfo.normal);
+            rotation *= Quaternion.Euler(0f, 0f, Random.Range(0, 360f));
+            var offset = 0.01f * hitInfo.normal;
+            Instantiate(decalPrefab, position + offset, rotation);
         }
     }
 }
